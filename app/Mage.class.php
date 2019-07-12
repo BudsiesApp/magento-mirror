b35a84b636b80afe42d32f4ad5e837a758506227
Kochnov Denis <d.kochnov.optimus@gmail.com>
2019-07-12 13:29:06 +0300

    Refs #18667: Add lost changes after merge

--- app/Mage.class.php
+++ app/Mage.class.php
@@ -143,7 +143,7 @@ final class Mage
             'major'     => '1',
             'minor'     => '9',
             'revision'  => '4',
-            'patch'     => '1',
+            'patch'     => '2',
             'stability' => '',
             'number'    => '',
         );
@@ -318,7 +318,6 @@ final class Mage
      *
      * @param string $path
      * @param mixed $store
-     *
      * @return mixed
      */
     public static function getStoreConfig($path, $store = null)
@@ -786,9 +785,9 @@ final class Mage
             ',',
             (string) self::getConfig()->getNode('dev/log/allowedFileExtensions', Mage_Core_Model_Store::DEFAULT_CODE)
         );
-        $logValidator = new Zend_Validate_File_Extension($_allowedFileExtensions);
         $logDir = self::getBaseDir('var') . DS . 'log';
-        if (!$logValidator->isValid($logDir . DS . $file)) {
+        $validatedFileExtension = pathinfo($file, PATHINFO_EXTENSION);
+        if (!$validatedFileExtension || !in_array($validatedFileExtension, $_allowedFileExtensions)) {
             return;
         }
 
@@ -823,6 +822,7 @@ final class Mage
                 $message = print_r($message, true);
             }
 
+            $message = addcslashes($message, '<?');
             $loggers[$file]->log($message, $level);
         }
         catch (Exception $e) {
